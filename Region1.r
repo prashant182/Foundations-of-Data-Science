2af07a4fa58ffff2a06e8911635cdb0548164f41
prashant182 <pnp249@nyu.edu>
2016-11-20 22:12:19 -0500

    Prashant Push
    Prashnt Push

--- Region1.r
+++ Region1.r
@@ -1,25 +1,38 @@
 
 library(dplyr)
 library(reshape)
-library(reshape2)
+library(ggplot2)
+
 library(plyr)
 
 Northeast
 
-abc
-
-
-NE.states<-ddply(Northeast,"CHSI_State_Name",numcolwise(sum))
-NE.states$State_FIPS_Code<-NULL
-NE.states$Population_Density<-NULL
+NE.states<-Northeast
 
 NE.Procedures<-data.frame()
-NE.Procedures<-NE.states[,c("All_Death","Major_Depression","Recent_Drug_Use","No_Exercise","Few_Fruit_Veg","Obesity","High_Blood_Pres","Smoker","Diabetes")]
+NE.Procedures<-NE.states[,c("No_Exercise","Few_Fruit_Veg","Obesity","High_Blood_Pres","Smoker","Diabetes")]
 NE.Procedures.Matrix<-as.matrix(NE.Procedures)
+NE.Cor<-cor(NE.Procedures)
+NE.Melt<-melt(data=NE.Cor,varnames = c("x","y"))
+NE.Melt <- NE.Melt[order(NE.Melt$value),]
+mean(NE.Melt$value)
+summary(NE.Melt)
+NE.Melt<-NE.Melt[(!NE.Melt$value==1),]
+NE.MeltMean<-mean(NE.Melt$value)
+grep(pattern = "green",x = colors(),value = T)
 
-NE.Melt<-melt(NE.Procedures.Matrix)
-NE.Melt<-ddply(NE.Melt$X2,.(variable), transform,  rescale = rescale(value),na.action=na.omit(()))
-NE.heatmap<-heatmap(NE.Melt, Rowv=NA, Colv=NA, col = heat.colors(256), scale="column", margins=c(5,10))
 
-
+# Heat map - using blues
+plt_heat_blue <- ggplot(data = NE.Melt, aes(x=x, y = y)) +
+  theme(panel.background = element_rect(fill = "snow2")) +
+  geom_tile(aes(fill = value)) + 
+  scale_fill_gradient2(low = "lightcyan", mid = "lightblue", high="midnightblue", 
+                       midpoint = NE.MeltMean,
+                       limits = c(0, 0.61), name = "Correlations") +
+  scale_x_discrete(expand = c(0,0)) + 
+  scale_y_discrete(expand = c(0,0)) +
+  labs(x=NULL, y=NULL) + 
+  theme(panel.background = element_rect(fill = "snow2")) +
+  ggtitle("Heat map of correlations in Risk Factors data") 
+plt_heat_blue 
 
